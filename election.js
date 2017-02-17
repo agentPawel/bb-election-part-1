8b704a13273f0160de0fa19898d45be65ba443a0
Pawel Parafianowicz <pawelp@outlook.com>
2017-02-17 15:30:57 -0500

    Assignment complete

--- election.js
+++ election.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
 
-  // Imagination!
+  $('<ul>').on
 
+  $.ajax({
+    method: 'GET',
+    url: 'https://bb-election-api.herokuapp.com/',
+    dataType: 'JSON'
+  }).done(function(data){
+    for(var i=0; i < data.candidates.length; i++) {
+     $('#list').append('<li> Name: ' + data.candidates[i].name + '</li>')
+     $('#list').append('<li> Votes: ' + data.candidates[i].votes + '</li><br>')
+    };
+  });
 });
